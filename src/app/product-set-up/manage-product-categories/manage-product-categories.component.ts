d933744b208cec773b037b9c0f15473463a3bfbc
joebidden1273@gmail.com <joebidden1273@gmail.com>
2022-07-27 01:36:58 +0300

    manage-product-categories

--- src/app/product-set-up/manage-product-categories/manage-product-categories.component.ts
+++ src/app/product-set-up/manage-product-categories/manage-product-categories.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, NgZone, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-manage-product-categories',
   templateUrl: './manage-product-categories.component.html',
   styleUrls: ['./manage-product-categories.component.css']
 })
+
+@HostListener('window: resize', ['$event'])
 export class ManageProductCategoriesComponent implements OnInit {
 
-  constructor() { }
+  public innerHeight: any;
 
-  ngOnInit(): void {
+  getScreenHeight() {
+    this.innerHeight = window.innerHeight + 'px';
   }
 
+  constructor(private ngZone: NgZone) {
+    window.onresize = (e) => {
+      this.ngZone.run(() => {
+        this.innerHeight = window.innerHeight + 'px';
+      });
+    };
+    this.getScreenHeight();
+  }
+
+  ngOnInit() {
+  }
+
+  onResize(event) {
+    this.innerHeight = event.target.innerHeight + 'px';
+  }
 }
+
